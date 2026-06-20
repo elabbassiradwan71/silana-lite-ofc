fcbdf8e4ee83a9102306348072940bdbd3f52e40
elabbassiradwan71 <elabbassiradwan710@gmail.com>
2026-06-20 14:39:59 +0100

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -14,8 +14,8 @@ global.setting = {
  }
 
 global.info = {
- nomerbot : '212605784394',
- pairingNumber : '212605784394',
+ nomerbot : '380916466121,
+ pairingNumber : '380916466121',
  figlet: 'silanalite', // create a start console display
  nomorwa : '212605784394',
  nameown : 'NOUREDDINE',
